dd97de919211c7ce728c366cda3f7d2c3fe8caf4
seeeedn <jonas.m1304@gmail.com>
2025-04-03 12:25:53 +0200

    added basic text editing

--- src/gapbuffer.rs
+++ src/gapbuffer.rs
@@ -1,4 +1,6 @@
-use std::usize;
+use std::io;
+use std::{fs, usize, vec};
+use std::fs::File;
 
 const INIT_SIZE: usize = 1024;
 
@@ -10,8 +12,8 @@ pub struct GapBuffer {
 }
 
 impl GapBuffer {
-    fn init_buffer(size: usize) -> Self {
-        let mut buffer: Vec<char> = vec![' '; size];
+    pub fn init_buffer() -> Self {
+        let mut buffer = vec![' '; INIT_SIZE];
         Self {
             data: buffer,
             total: INIT_SIZE,
@@ -20,21 +22,21 @@ impl GapBuffer {
         }
     }
 
-    fn expand_buffer(&mut self) {
+    pub fn expand_buffer(&mut self) {
         let new_total = self.total * 2;
         let after_buffer = self.data.len() - self.gap_end;
-
+        
         self.data.resize(new_total, ' ');
 
         let new_gap_end = new_total - after_buffer;
 
-        self.data.copy_within(self.gap_end.., new_gap_end);
+        self.data.copy_within(self.gap_end..self.total, new_gap_end);
 
         self.gap_end = new_gap_end;
         self.total = new_total;
     }
 
-    fn insert_char(&mut self, ch: char) {
+    pub fn insert_char(&mut self, ch: char) {
         if self.gap_start == self.gap_end - 1 {
             self.expand_buffer();
         }
@@ -45,19 +47,19 @@ impl GapBuffer {
         }
     }
 
-    fn delete_char(&mut self) {
+    pub fn delete_char(&mut self) {
         if self.gap_start > 0 {
             self.gap_start -= 1;
         }
     }
 
-    fn delete_char_forward(&mut self) {
+    pub fn delete_char_forward(&mut self) {
         if self.gap_end < self.total - 1 {
             self.gap_end += 1;
         }
     }
 
-    fn move_right(&mut self, offset: usize) {
+    pub fn move_right(&mut self, offset: usize) {
         if offset <= self.gap_start || self.gap_end + offset >= self.total {
             return;
         }
@@ -68,18 +70,43 @@ impl GapBuffer {
         self.gap_end += offset;
     }
 
-    fn move_left(&mut self, offset: usize) {
+    pub fn move_left(&mut self, offset: usize) {
         if self.gap_start < offset {
             return;
         }
 
-        self.data.copy_within(self.gap_start - offset..self.gap_start - 1, self.gap_end - offset);
+        self.data.copy_within(self.gap_start - offset..self.gap_start - offset + 1, self.gap_end - offset);
 
         self.gap_start -= offset;
         self.gap_end -= offset;
     }
 
-    fn move_gap(&mut self, offset: usize) {
-        
+    pub fn move_gap(&mut self, offset: usize) {
+        if offset > self.gap_start {
+            self.move_right(offset);
+        }
+        else if offset < self.gap_start {
+            self.move_left(offset);
+        }
+    }
+
+    pub fn data_to_string(&self) -> String {
+        let mut string: String = self.data.iter().collect();
+        string.replace_range(self.gap_start..self.gap_end, "");
+        string
+    }
+
+    pub fn read_from_file(&mut self, path: &str) -> io::Result<()> {
+        let contents = fs::read_to_string(path)?;
+        for i in contents.chars() {
+            self.insert_char(i);
+        }
+        Ok(())
+    }
+
+    pub fn write_to_file(&self, path: &str) -> io::Result<()> {
+        let write_data = self.data_to_string();
+        fs::write(path, write_data)?;
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
+use std::io;
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{layout::Position, style::{Style, Stylize}, text::Text, DefaultTerminal};
+
 mod gapbuffer;
 
-fn main() {
-    println!("Hello, World!")
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let result = run(&mut terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    let mut style = Style::new().white();
+    let mut gb = gapbuffer::GapBuffer::init_buffer();
+    let _ = gb.read_from_file("src/main.rs");
+
+    loop {
+        terminal.draw(|f| {
+            let size = f.area();
+            let text = Text::raw(gb.data_to_string()).style(style);
+            f.set_cursor_position(Position::new(0, 0));
+            f.render_widget(text, size);
+        })?;
+
+        if let Event::Key(key ) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Esc => {
+                        //let _ = gb.write_to_file("src/main.rs");
+                        break;
+                    }
+                    KeyCode::Enter => gb.insert_char('\n'),
+                    KeyCode::Backspace => gb.delete_char(),
+                    KeyCode::Left => gb.move_left(1),
+                    KeyCode::Char(ch) => gb.insert_char(ch),
+                    _ => style = style.green(),
+                }
+            }
+        }
+    }
+    Ok(())
 }
